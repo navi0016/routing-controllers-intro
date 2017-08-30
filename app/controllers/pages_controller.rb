3209df71a706082d97332c3a2f59d0ac10c0a5fe
Navi Mann <navdeep.k.mann@gmail.com>
2017-08-30 15:01:06 -0400

    added dynamic kitten route and method to view kitten pic in user sepicifed size

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,4 +11,9 @@ def contest
   @header = "I will go on top"
 end
 
+def kitten
+  requested_size = params[:size]
+  @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+end
+
 end
